e33214dd2be7965760e85513a9ac4f8d7c076154
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-06-22 00:07:24 +0300

    Update Commit

---
+++ Java/Methods/src/PalindromeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = scanner.nextInt();
+
+		if (isPalindrome(number)) {
+			System.out.println(number + " is a palindrome number.");
+		} else {
+			System.out.println(number + " is not a palindrome number.");
+		}
+
+		scanner.close();
+	}
+
+	static boolean isPalindrome(int num) {
+		int originalNumber = num;
+		int reverseNumber = 0;
+		while (num != 0) {
+			int remainder = num % 10;
+			reverseNumber = reverseNumber * 10 + remainder;
+			num = num / 10;
+		}
+		return originalNumber == reverseNumber;
+	}
+
+}
