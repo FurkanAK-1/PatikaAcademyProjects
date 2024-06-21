f2ec0d43c7083800ef11971bf40e0ed9cfc217f2
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-06-21 22:43:39 +0300

    Prime Numbers

---
+++ Java/Conditional_Statements_and_Code_Blocks/src/PrimeNumbers.java
@@ -0,0 +1,26 @@
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Prime numbers between 1 and 100:");
+
+		for (int number = 2; number <= 100; number++) {
+			if (isPrime(number)) {
+				System.out.print(number + " ");
+			}
+		}
+	}
+
+	public static boolean isPrime(int number) {
+		if (number <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(number); i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
