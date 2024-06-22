0917b24cc99102a2721e7f7780853b21eb83ec78
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-06-22 11:03:00 +0300

    Add Prime Number Recursive

---
+++ Java/Methods/src/PrimeNumberRecursive.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumberRecursive {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = scanner.nextInt();
+
+		if (isPrime(number, 2)) {
+			System.out.println(number + " is a PRIME number!");
+		} else {
+			System.out.println(number + " is NOT a PRIME number!");
+		}
+
+		scanner.close();
+	}
+
+	static boolean isPrime(int number, int divisor) {
+		if (number <= 2) {
+			return (number == 2);
+		}
+		if (number % divisor == 0) {
+			return false;
+		}
+		if (divisor * divisor > number) {
+			return true;
+		}
+		return isPrime(number, divisor + 1);
+	}
+}
