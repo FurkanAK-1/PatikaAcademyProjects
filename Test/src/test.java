bc3a21350551f2e84f3a488f650379e976c271dc
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-07-22 15:03:08 +0300

    Update

--- Test/src/test.java
+++ Test/src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("deneme test");
+		System.out.println("denessme test");
 
 	}
 
