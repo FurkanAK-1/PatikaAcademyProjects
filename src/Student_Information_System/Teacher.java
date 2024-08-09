4431f0eef2d2895abdfed0071de5854c428e8d61
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-08-09 13:31:29 +0300

    Update Commit

---
+++ src/Student_Information_System/Teacher.java
@@ -0,0 +1,13 @@
+package Student_Information_System;
+
+public class Teacher {
+	String name;
+	String phone;
+	String branch;
+
+	public Teacher(String name, String phone, String branch) {
+		this.name = name;
+		this.phone = phone;
+		this.branch = branch;
+	}
+}
