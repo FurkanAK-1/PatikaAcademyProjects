e943f9836b49e8976441706fc3b9b55be9f1234b
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-08-09 13:35:10 +0300

    Update

--- Java/Methods/src/RecursivePattern.java
+++ Java/Methods/src/RecursivePattern.java
@@ -4,7 +4,7 @@ public class RecursivePattern {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number....: ");
         int number = scanner.nextInt();
         
         recursivePattern(number, number, false);
