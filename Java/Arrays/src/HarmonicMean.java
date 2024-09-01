248092710becbda993c96ad5778886916a87f43e
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-09-01 17:55:54 +0300

    Commit HarmonicMean

---
+++ Java/Arrays/src/HarmonicMean.java
@@ -0,0 +1,17 @@
+
+public class HarmonicMean {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 4, 8 }; // Örnek dizi
+		double harmonicMean = calculateHarmonicMean(numbers);
+		System.out.println("Dizideki sayıların harmonik ortalaması: " + harmonicMean);
+	}
+
+	public static double calculateHarmonicMean(int[] numbers) {
+		double sum = 0.0;
+		for (int number : numbers) {
+			sum += 1.0 / number;
+		}
+		return numbers.length / sum;
+	}
+}
